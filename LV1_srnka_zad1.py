b4600afd991c969b7451545d3b040a4d563530cd
TaktEkspanzije <jetamsrnka@gmail.com>
2026-03-13 13:58:40 +0100

    Delete LV1_srnka_zad1.py

--- LV1_srnka_zad1.py
+++
@@ -1,9 +0,0 @@
-def total_euro(sati, eura_po_satu):
-    return sati * eura_po_satu
-
-sati = float(input("Radnih sati: "))
-eura_po_satu = float(input("euro/h: "))
-
-ukupno = total_euro(sati, eura_po_satu)
-
-print(f"Ukupno: {ukupno} eura")
